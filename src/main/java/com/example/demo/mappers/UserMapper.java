f31e7d1990dfb03ad7eb74401a6889d64f379601
lgtLeonardo <43980857+lgtLeonardo@users.noreply.github.com>
2019-05-01 20:04:57 -0300

    delete XML and the add annotation query configuration in UserMApper interface

--- src/main/java/com/example/demo/mappers/UserMapper.java
+++ src/main/java/com/example/demo/mappers/UserMapper.java
@@ -2,10 +2,22 @@ package com.example.demo.mappers;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+
 import com.example.demo.domain.User;
 
 public interface UserMapper {
+	
+	@Insert("insert into users(name,email) values (#{userName}, #{userEmail})")
+	@SelectKey(statement = "call identity()", keyProperty = "userId", 
+				before = false, resultType = Integer.class)
 	void insertUser(User user);
+	
+	@Select("select id, name, email from users")
 	List<User> findAllUsers();
-	User findUserById(Integer id);
+	
+	@Select("select id, name, email from users WHERE id=#{id}")
+	User findUserById(Integer userId);
 }
